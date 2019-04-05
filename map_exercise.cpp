50479e1de5aac86c607763856fa7b99a6ab65ce0
Carlo de Falco <carlo.defalco@polimi.it>
2019-04-05 12:01:32 +0200

    added code with a bug

---
+++ map_exercise.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <map>
+#include <random>
+
+
+int
+main ()
+{
+  std::map<int, double> m;
+
+  std::random_device generator;
+  std::uniform_int_distribution<int> intdist (0, 100);
+  std::uniform_real_distribution<double> doubdist (0.0, 1.0);
+
+  for (int ii = 0; ii < 10; ++ii)
+    m[intdist (generator)] = doubdist (generator);
+
+  std::cout << "initial contents" << std::endl;
+  for (auto& jj : m)
+    std::cout << jj.first << " " << jj.second << std::endl;
+
+  for (auto jj = m.begin (); jj != m.end (); ++jj)
+    if (jj->second < .5)
+      m.erase (jj);
+
+  std::cout << "final contents" << std::endl;
+  for (auto& jj : m)
+    std::cout << jj.first << " " << jj.second << std::endl;
+
+}
